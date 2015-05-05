028124a5872aa2e99016132b6db732d0242f6e41
Silwest <tyskie3>
2015-05-05 09:18:25 -0500

    [Sensor build] Build added, jar with all external librares, main class

---
+++ Sensor/src/main/java/sensor/Sensor.java
@@ -0,0 +1,21 @@
+package sensor;
+
+
+import org.developers.sensor.client.SensorClient;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Silwest
+ */
+public class Sensor {
+    public static void main(String[] args) {
+        System.setProperty("java.library.path", "C:/sensor/lib");
+        new SensorClient().start();
+    }
+}
